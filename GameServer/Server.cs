f781401ee97a8bb2441cbaf60df1b8e890436df5
Filip Maj <filipmaj@gmail.com>
2022-12-24 10:09:53 -0500

    Gameserver is actually TCP

--- GameServer/Server.cs
+++ GameServer/Server.cs
@@ -1,20 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace IWANGOEmulator.GameServer
 {
     class Server
     {
-        public const int UDP_PORT = 0x2F2F;
+        public const int PORT = 9502;
         public const int BUFFER_SIZE = 0x200;
         public const int BACKLOG = 100;
 
-        private Socket UdpSocket;
-        private EndPoint RemoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0x2F2F);
-        private byte[] Buffer = new byte[0xFFFF];
-
-        private Object state = new Object();
+        private Socket serverSocket;
+        private List<ClientConnection> connList = new List<ClientConnection>();
 
         public Server()
         {
@@ -23,39 +22,106 @@ namespace IWANGOEmulator.GameServer
         #region Socket Handling
         public bool StartServer()
         {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("0.0.0.0"), PORT);
+
             try
             {
-                UdpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                UdpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
-                UdpSocket.Bind(new IPEndPoint(IPAddress.Parse("0.0.0.0"), UDP_PORT));
-               // UdpSocket.BeginReceiveFrom(Buffer, 0, Buffer.Length, SocketFlags.None, ref RemoteEP, ReceiveCallback, state);
-                return true;
+                serverSocket = new Socket(serverEndPoint.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                serverSocket.Bind(serverEndPoint);
+                serverSocket.Listen(BACKLOG);
+                serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), serverSocket);
             }
-            catch (SocketException e)
+            catch (Exception e)
             {
-                Program.Log.Error("There was an issue setting up the socket");
+                Program.Log.Error("Could not create server socket: " + e.Message);
             }
-            return false;
+
+            Program.Log.Info("Server has started @ {0}:{1}", (serverSocket.LocalEndPoint as IPEndPoint).Address, (serverSocket.LocalEndPoint as IPEndPoint).Port);
+
+            return true;
+        }
+
+        public void SendAll(byte[] data)
+        {
+            foreach (ClientConnection conn in connList)
+            {
+                conn.Send(data);
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult result)
+        {
+            ClientConnection conn = null;
+            try
+            {
+                Socket s = (Socket)result.AsyncState;
+                conn = new ClientConnection
+                {
+                    socket = s.EndAccept(result),
+                    buffer = new byte[BUFFER_SIZE]
+                };
+                lock (connList)
+                {
+                    connList.Add(conn);
+                    Program.Log.Info($"Got a connection from {conn.GetAddress()}");
+                }
+
+                conn.socket.BeginReceive(conn.buffer, 0, conn.buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), conn);
+            }
+            catch (SocketException)
+            {
+                if (conn.socket != null)
+                {
+                    conn.socket.Close();
+                    lock (connList)
+                    {
+                        connList.Remove(conn);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (conn.socket != null)
+                {
+                    conn.socket.Close();
+                    lock (connList)
+                    {
+                        connList.Remove(conn);
+                    }
+                }
+            }
+
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), serverSocket);
         }
 
         private void ReceiveCallback(IAsyncResult result)
         {
-            Object conn = result.AsyncState;
+            ClientConnection conn = (ClientConnection)result.AsyncState;
 
-            int bytes = UdpSocket.EndReceiveFrom(result, ref RemoteEP);
-            UdpSocket.BeginReceiveFrom(Buffer, 0, Buffer.Length, SocketFlags.None, ref RemoteEP, ReceiveCallback, state);
-            Program.Log.Info($"Received {bytes} bytes");
+            try
+            {
+                int bytesRead = conn.socket.EndReceive(result);
+                bytesRead += conn.lastPartialSize;
+
+                if (bytesRead > 0)
+                {
+                    Program.Log.Info($"Read {bytesRead} bytes");
+                }
+            }
+            catch (SocketException)
+            {
+                if (conn.socket != null)
+                {
+                    Program.Log.Info("{0} has disconnected.", conn.GetAddress());
+
+                    lock (connList)
+                    {
+                        connList.Remove(conn);
+                    }
+                }
+            }
         }
 
         #endregion
-
-        public void SendAll(byte[] data)
-        {
-                UdpSocket.BeginSendTo(data, 0, data.Length, SocketFlags.None, RemoteEP, (result) => {
-                Object obj = result.AsyncState;
-                int bytes = UdpSocket.EndSend(result);
-                Program.Log.Info($"Sent {bytes} bytes");
-            }, state);
-        }
     }
 }
